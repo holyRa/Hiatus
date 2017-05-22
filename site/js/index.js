2c07f7c5b48b130a07a14fa8fc9aec6a799452ef
holyRa <rasmusbeck2k@outlook.com>
2017-05-22 18:31:09 +0200

    Revert "Finalized index page"
    This reverts commit 0d79a60e038e65ede6f38b144e374968458dcfce.

--- site/js/index.js
+++ site/js/index.js
@@ -1,4 +1,3 @@
-//Fix header
 var headerHeight = $("header").outerHeight();
 
 $("#dividerimgdiv").css("padding-top", headerHeight);
@@ -6,11 +5,3 @@ $(window).resize(function() {
     headerHeight = $("header").outerHeight();
     $("#dividerimgdiv").css("padding-top", headerHeight);
 });
-
-//Make content links fade to blue on hover
-$(".content a").mouseover(function() {
-    $(this).animate({ color: "#0099ff"}, 200);
-}).mouseout(function() {
-    $(this).animate({ color: "#5B6073"}, 200);
-})
-
