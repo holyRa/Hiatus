0d79a60e038e65ede6f38b144e374968458dcfce
holyRa <rasmusbeck2k@outlook.com>
2017-05-22 18:30:59 +0200

    Finalized index page

--- site/js/index.js
+++ site/js/index.js
@@ -1,3 +1,4 @@
+//Fix header
 var headerHeight = $("header").outerHeight();
 
 $("#dividerimgdiv").css("padding-top", headerHeight);
@@ -5,3 +6,11 @@ $(window).resize(function() {
     headerHeight = $("header").outerHeight();
     $("#dividerimgdiv").css("padding-top", headerHeight);
 });
+
+//Make content links fade to blue on hover
+$(".content a").mouseover(function() {
+    $(this).animate({ color: "#0099ff"}, 200);
+}).mouseout(function() {
+    $(this).animate({ color: "#5B6073"}, 200);
+})
+
