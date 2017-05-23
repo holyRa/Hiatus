201630cfe3045d969130196437fdbc7d8b66df32
holyRa <rasmusbeck2k@outlook.com>
2017-05-23 17:47:15 +0200

    forgot this one in previous commit

--- site/js/main.js
+++ site/js/main.js
@@ -1,11 +1,19 @@
 /*
 This file contains JS for:
 Header
-
 */
 
 var scrollThreshold = $("header").outerHeight();
 $(window).scroll(function(event) {
     var alpha = 1 - (0.2 * (Math.min($(window).scrollTop(), scrollThreshold) / scrollThreshold)); //For slow-ish fade-in fuckery - fades alpha from 1 to 0.8
     $("header").css({ backgroundColor: "rgba(0, 0, 0, " + alpha + ")"});
+});
+
+//Fixes header distancing
+var headerHeight = $("header").outerHeight();
+
+$("#dividerimgdiv").css("padding-top", headerHeight);
+$(window).resize(function() {
+    headerHeight = $("header").outerHeight();
+    $("#dividerimgdiv").css("padding-top", headerHeight);
 });
