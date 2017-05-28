d0c6365d03dab2c304e845d509797084adfa7f86
holyRa <rasmusbeck2k@outlook.com>
2017-05-28 14:01:13 +0200

    Almost finished up News page
    Just have to make me a lightbox!

---
+++ site/js/news.js
@@ -0,0 +1,9 @@
+$(".videobutton").mouseover(function() {
+    $(this).stop().animate({ color: "#99A1BF", borderColor: "#99A1BF"}, 100);
+}).mouseout(function() {
+    $(this).stop().animate({ color: "#5B6073", borderColor: "#5B6073"}, 100);
+});
+
+function notAvailable() {
+    alert("We regret to inform you that this kill video is not available.");
+}
