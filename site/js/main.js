0838c28b83d9846ade3870dc7d270a94d71eefc3
holyRa <rasmusbeck2k@outlook.com>
2017-06-03 15:46:29 +0200

    Started work to make site mobile-friendlty

--- site/js/main.js
+++ site/js/main.js
@@ -3,17 +3,19 @@ This file contains JS for:
 Header
 */
 
-var scrollThreshold = $("header").outerHeight();
-$(window).scroll(function(event) {
-    var alpha = 1 - (0.2 * (Math.min($(window).scrollTop(), scrollThreshold) / scrollThreshold)); //For slow-ish fade-in fuckery - fades alpha from 1 to 0.8
-    $("header").css({ backgroundColor: "rgba(0, 0, 0, " + alpha + ")"});
-});
+if($(window).width > 670) { //Only apply header js if we're using the position: fixed header
+    var scrollThreshold = $("header").outerHeight();
+    $(window).scroll(function(event) {
+        var alpha = 1 - (0.2 * (Math.min($(window).scrollTop(), scrollThreshold) / scrollThreshold)); //For slow-ish fade-in fuckery - fades alpha from 1 to 0.8
+        $("header").css({ backgroundColor: "rgba(0, 0, 0, " + alpha + ")"});
+    });
 
-//Fixes header distancing
-var headerHeight = $("header").outerHeight();
+    //Fixes header distancing
+    var headerHeight = $("header").outerHeight();
 
-$("#dividerimgdiv").css("padding-top", headerHeight);
-$(window).resize(function() {
-    headerHeight = $("header").outerHeight();
     $("#dividerimgdiv").css("padding-top", headerHeight);
-});
+    $(window).resize(function() {
+        headerHeight = $("header").outerHeight();
+        $("#dividerimgdiv").css("padding-top", headerHeight);
+    });
+}
