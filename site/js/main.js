889e84bf1f8fd2d0a76ac7c5d8af136a680ce61a
holyRa <rasmusbeck2k@outlook.com>
2017-06-03 17:02:15 +0200

    Fixed header js effects being applied incorrectly

--- site/js/main.js
+++ site/js/main.js
@@ -3,19 +3,22 @@ This file contains JS for:
 Header
 */
 
-if($(window).width > 670) { //Only apply header js if we're using the position: fixed header
-    var scrollThreshold = $("header").outerHeight();
-    $(window).scroll(function(event) {
+var scrollThreshold = $("header").outerHeight();
+$(window).scroll(function(event) {
+
+    if($(window).width() > 670) { //Only apply header js if using the position: fixed header
         var alpha = 1 - (0.2 * (Math.min($(window).scrollTop(), scrollThreshold) / scrollThreshold)); //For slow-ish fade-in fuckery - fades alpha from 1 to 0.8
         $("header").css({ backgroundColor: "rgba(0, 0, 0, " + alpha + ")"});
-    });
+    } else {
+        $("header").css({ backgroundColor: "rgba(0, 0, 0, 1)"});
+    }
+});
 
-    //Fixes header distancing
-    var headerHeight = $("header").outerHeight();
+//Fixes header distancing
+var headerHeight = $(window).width() > 670 ? $("header").outerHeight() : 0; //Only apply padding if using the position: fixed header
 
+$("#dividerimgdiv").css("padding-top", headerHeight);
+$(window).resize(function() {
+    headerHeight = $(window).width() > 670 ? $("header").outerHeight() : 0; 
     $("#dividerimgdiv").css("padding-top", headerHeight);
-    $(window).resize(function() {
-        headerHeight = $("header").outerHeight();
-        $("#dividerimgdiv").css("padding-top", headerHeight);
-    });
-}
+});
